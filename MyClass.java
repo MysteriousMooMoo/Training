654bbb6594834c483058885f8228a20b299dc166
MysteriousMooMoo <95538692+MysteriousMooMoo@users.noreply.github.com>
2021-12-04 10:58:48 -0600

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    static int i = 1;
+    static int o = 3;
+   
+    public static void ThingAMaJig() {
+        System.out.println("This is my favorite number: " + i);
+        System.out.println("This is my second favorite number: " + o);
+    }
+
+    public static void main(String[] args) {
+        ThingAMaJig();
+    }
+}
